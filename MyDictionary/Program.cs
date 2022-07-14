a948025d598a20863f744dde097fc076a8d53225
Tahiri Fidan <tahirifdn@gmail.com>
2022-07-14 15:13:18 +0300

    MyDictionary oluşturuldu

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
