fc974ae3a04e42e8208f60448d761f01ff9dfb4a
Tahiri Fidan <tahirifdn@gmail.com>
2022-07-16 14:35:55 +0300

    Bazı İyileştirmeler yapıldı

--- MyDictionary/MyDictionary.cs
+++ MyDictionary/MyDictionary.cs
@@ -16,15 +16,48 @@ namespace MyDictionary
         }
         public void Add(T _key,K _value)
         {
+            if (Control(_key))
+            {
+                Configuration(_key);
+                items[items.Length - 1] = new KeyValuePair<T, K>(_key, _value); // En son bize gelen değeri dizimize aktarıp işi bitirdik.
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz key değeri özel olmalı. Yani daha önceden eklenmiş bir key değerini tekrardan kullanamassınız.");
+            }
+
+        }
+
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+        }
+
+        private void Configuration(T _key)
+        {
             KeyValuePair<T, K>[] tempArray = items;  // Geçici dizimizi oluşturduk
-            items = new KeyValuePair<T, K>[items.Length +1];
+            items = new KeyValuePair<T, K>[items.Length + 1];
             for (int i = 0; i < tempArray.Length; i++)
             {
                 items[i] = tempArray[i];  //Geçici dizideki elemanları alıp asıl dizimize gri koyduk.
             }
-
-            items[items.Length-1] = new KeyValuePair<T, K>(_key, _value); // En son bize gelen değeri dizimize aktarıp işi bitirdik.
-
+            
+        }
+        private bool Control(T _Key)
+        {
+            bool control = true;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Key.ToString() == _Key.ToString())
+                {
+                    control = false;
+                }
+            }
+            return control;
         }
     }
 }
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -8,7 +8,10 @@ namespace MyDictionary
         {
            MyDictionary<int,string> names = new MyDictionary<int,string>();
            names.Add(1, "Tahiri");
-           names.Add(1,"Engin");
+           names.Add(1, "Engin"); // Bunu eklemye çalışırken bize br hata verecek.
+           names.Add(2, "Engin"); // Burda hata vermez çünkü key değeri daha önce kullanılmamış.
+
+           names.ShowList();
         }
     }
 }
